857676f3e0216aff9b2503194bef59bb8b380fd8
NguyenLang <nguyen.lang2505@gmail.com>
2020-11-30 17:50:12 +0700

    30 Nov

---
+++ RemoteCenter/YUClientSelenium/ConfigVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YUClientSelenium
+{
+    class ConfigVariable
+    {
+
+    }
+
+    class ElementIdentify
+    {
+        public const String likeXpath = "//*[@id=\"top-level-buttons\"]/ytd-toggle-button-renderer[1]";
+        public const String disLikeXpath = "//*[@id=\"top-level-buttons\"]/ytd-toggle-button-renderer[2]";
+        public const String shareXpath = "//*[@id=\"top-level-buttons\"]/ytd-button-renderer[1]";
+        public const String saveXpath = "//*[@id=\"top-level-buttons\"]/ytd-button-renderer[2]";
+        public const String subcribeXpath = "//*[@id=\"subscribe-button\"]";
+        public const String subcribeTextXpath = "//*[@id=\"subscribe-button\"]/ytd-subscribe-button-renderer/paper-button/yt-formatted-string";
+
+        public const String watchList = "//*[@id=\"playlists\"]/ytd-playlist-add-to-option-renderer[1]";
+
+        //for comment
+        public const String cmtPositionId = "simplebox-placeholder";
+        public const String cmtEditBoxId = "contenteditable-root";
+        public const String cmtSubmitBtn = "submit-button";
+
+        //for login
+        public const String loginEditBoxId = "identifierId";
+        public const String loginNexId = "identifierNext";
+    }
+}
---
+++ RemoteCenter/YUClientSelenium/Extention.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YUClientSelenium
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return driver.FindElement(by);
+        }
+
+        public static IWebElement WaitUntilVisible(this IWebDriver driver, By itemSpecifier, int secondsTimeout = 10)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, secondsTimeout));
+            var element = wait.Until<IWebElement>(drv =>
+            {
+                try
+                {
+                    var elementToBeDisplayed = drv.FindElement(itemSpecifier);
+                    if (elementToBeDisplayed.Displayed)
+                    {
+                        return elementToBeDisplayed;
+                    }
+                    return null;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return null;
+                }
+                catch (NoSuchElementException)
+                {
+                    return null;
+                }
+
+            });
+            return element;
+        }
+    }
+}
